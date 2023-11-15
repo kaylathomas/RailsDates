6db7bd231c88d05f3ff806d1d8efffeb935540e4
KayLa <35053868+kaylathomas@users.noreply.github.com>
2023-11-15 00:04:34 -0500

    Fixed .ago issues.

--- RailsDates/timeint.py
+++ RailsDates/timeint.py
@@ -4,51 +4,73 @@ class TimeDeltaBuilder:
     """Internal class to build timedelta objects based on the called attribute."""
     def __init__(self, value):
         self.value = value
+        self.delta = timedelta()
 
+    @property
     def minute(self):
-        return timedelta(minutes=self.value)
+        self.delta += timedelta(minutes=self.value)
+        return self
 
+    @property
     def minutes(self):
-        return self.minute()
+        return self.minute
 
+    @property
     def hour(self):
-        return timedelta(hours=self.value)
+        self.delta += timedelta(hours=self.value)
+        return self
 
+    @property
     def hours(self):
-        return self.hour()
+        return self.hour
 
+    @property
     def day(self):
-        return timedelta(days=self.value)
+        self.delta += timedelta(days=self.value)
+        return self
 
+    @property
     def days(self):
-        return self.day()
+        return self.day
 
+    @property
     def week(self):
-        return timedelta(weeks=self.value)
+        self.delta += timedelta(weeks=self.value)
+        return self
 
+    @property
     def weeks(self):
-        return self.week()
+        return self.week
 
+    @property
     def month(self):
-        return timedelta(days=self.value * 30)  # approximation
+        self.delta += timedelta(days=self.value * 30)  # Approximation
+        return self
 
+    @property
     def months(self):
-        return self.month()
+        return self.month
 
+    @property
     def decade(self):
-        return timedelta(days=self.value * 365.25 * 10)  # approximation, considering leap years
+        self.delta += timedelta(days=self.value * 365.25 * 10)  # Approximation, considering leap years
+        return self
 
+    @property
     def decades(self):
-        return self.decade()
+        return self.decade
 
+    @property
     def leapyear(self):
-        return timedelta(days=self.value * 366)  # approximation for leap years
+        self.delta += timedelta(days=self.value * 366)  # Approximation for leap years
+        return self
 
+    @property
     def leapyears(self):
-        return self.leapyear()
+        return self.leapyear
 
     def ago(self):
-        return datetime.utcnow() - self.value()
+        return datetime.utcnow() - self.delta
 
 class TimeInt:
     def __init__(self, value):
@@ -58,28 +80,56 @@ class TimeInt:
 
     @property
     def minute(self):
-        return TimeDeltaBuilder(self.value)
+        return TimeDeltaBuilder(self.value).minute
+
+    @property
+    def minutes(self):
+        return self.minute
 
     @property
     def hour(self):
-        return TimeDeltaBuilder(self.value)
+        return TimeDeltaBuilder(self.value).hour
+
+    @property
+    def hours(self):
+        return self.hour
 
     @property
     def day(self):
-        return TimeDeltaBuilder(self.value)
+        return TimeDeltaBuilder(self.value).day
+
+    @property
+    def days(self):
+        return self.day
 
     @property
     def week(self):
-        return TimeDeltaBuilder(self.value)
+        return TimeDeltaBuilder(self.value).week
+
+    @property
+    def weeks(self):
+        return self.week
 
     @property
     def month(self):
-        return TimeDeltaBuilder(self.value)
+        return TimeDeltaBuilder(self.value).month
+
+    @property
+    def months(self):
+        return self.month
 
     @property
     def decade(self):
-        return TimeDeltaBuilder(self.value)
+        return TimeDeltaBuilder(self.value).decade
+
+    @property
+    def decades(self):
+        return self.decade
 
     @property
     def leapyear(self):
-        return TimeDeltaBuilder(self.value)
+        return TimeDeltaBuilder(self.value).leapyear
+
+    @property
+    def leapyears(self):
+        return self.leapyear
